ee6441d8564242c7d897d858da3d1404bfc84b3b
penguo <ireninpr@gmail.com>
2018-03-28 17:31:06 +0900

    syscall modified

--- src/userprog/syscall.c
+++ src/userprog/syscall.c
@@ -3,15 +3,38 @@
 #include <syscall-nr.h>
 #include "threads/interrupt.h"
 #include "threads/thread.h"
+#include <devices/shutdown.h>
+#include <filesys/filesys.h>
 
 static void syscall_handler (struct intr_frame *);
 void check_address(void *addr);
 void get_argument(void *esp, int *arg, int count);
+void halt(void);
+void exit(int status);
+bool create(const char* file, unsigned inital_size);
+bool remove(const char* file);
 
 void
 syscall_init (void) 
 {
   intr_register_int (0x30, 3, INTR_ON, syscall_handler, "syscall");
+}
+
+void halt(void){
+  shutdown_power_off();
+}
+
+void exit(int status){
+  printf("%s: exit(%d)\n", thread_name(), status);
+  thread_exit();
+}
+
+bool create(const char* file, unsigned initial_size){
+  return filesys_create(file, initial_size);
+}
+
+bool remove(const char* file){
+  return filesys_remove(file);
 }
 
 void get_argument(void *esp, int *arg, int count)
@@ -42,26 +65,26 @@ static void
 syscall_handler (struct intr_frame *f UNUSED) 
 {
 // TODO
-  void* esp = f->esp;
-  int syscall_nr = (int)esp;
+  int syscall_nr = (int)f->esp;
   int arg[5]; // 사이즈는 마음대로, 상수값이니 define 하는게 더 좋을듯.*
   switch (syscall_nr){
     case SYS_HALT:
-//      halt();
+      halt();
       break;
     case SYS_EXIT:
-      get_argument(esp, arg, 1);
-//      f->eax = exit(arg[0]);
+      get_argument(f->esp, arg, 1);
+      exit(arg[0]);
+      f->eax = arg[0];
       break;
     case SYS_CREATE:
-      get_argument(esp, arg, 2);
+      get_argument(f->esp, arg, 2);
       check_address((void*)arg[0]);
-//      f->eax = create(arg[0], arg[1]);
+      f->eax = create((const char*)arg[0], (unsigned)arg[1]);
       break;
     case SYS_REMOVE:
-      get_argument(esp, arg, 1);
+      get_argument(f->esp, arg, 1);
       check_address((void*)arg[0]);
-//      f->eax = remove(arg[0]);
+      f->eax = remove((const char*)arg[0]);
       break;
   }
   thread_exit ();
