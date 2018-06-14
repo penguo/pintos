b0195fe4f3c5b58a6e12b8f7fcda57d62c63418d
penguo <penguo@hanyang.ac.kr>
2018-06-15 01:28:08 +0900

    다시 리셋, buffer_cache 다시 추가 (472p까지)

---
+++ README.md
@@ -0,0 +1,7 @@
+# 2018OS-pintos
+
+pintos project #3 virtual memory 중 스와핑 파트 까지 구현된 것은 swapping_backup에 있습니다.
+
+실행시 에러가 나서 따로 브랜치에 저장해 놓았습니다.
+
+master branch에는 test 10개 fail 상태인 mmap 파트까지 완성되어 있습니다.
---
+++ src/filesys/buffer_cache.h
@@ -0,0 +1,20 @@
+#include "filesys/filesys.h"
+#include "filesys/inode.h"
+#include "threads/synch.h"
+
+//buffer cache entry 를 관리
+struct buffer_head{
+	int dirty; //dirty
+
+	int used; //is used?
+
+	block_sector_t address; //disk sector 주소
+
+	int clock; //clock 알고리즘
+
+	struct lock l;//lock
+
+	void *buffer;
+}
+
+
